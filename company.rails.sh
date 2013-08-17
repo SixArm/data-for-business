ce72280ad8d9d33af2678a8fad14d89369bf58d1
SixArm <sixarm@sixarm.com>
2013-08-16 18:04:44 -0700

    Add company

---
+++ company.rails.sh
@@ -0,0 +1,9 @@
+generate scaffold company \
+name_en:string \
+abbr_en:string \
+domain_en:string \
+stock_exchange:references \
+stock_symbol:string \
+duns:integer \
+ein:integer \
+naics:integer \
